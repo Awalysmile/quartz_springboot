e722a81cb6bf6b123c54bf8c3a2500f0d0ec94ee
miles <awalsmile@qq.com>
2019-08-04 22:17:16 +0800

    "调度器去掉lambda表达式，提高可读性"

--- quartz/src/main/java/com/miles/quartz/handler/QuartzHandler.java
+++ quartz/src/main/java/com/miles/quartz/handler/QuartzHandler.java
@@ -6,7 +6,7 @@ import org.quartz.JobExecutionContext;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Optional;
+import java.lang.reflect.Method;
 
 import static com.miles.quartz.util.SpringUtil.getBeanByClazz;
 import static com.miles.quartz.util.SpringUtil.getBeanByName;
@@ -23,23 +23,23 @@ import static com.miles.quartz.util.SpringUtil.getBeanByName;
 public class QuartzHandler implements Job {
     @Override
     public void execute(JobExecutionContext context) {
+        Object object;
+        Method method = null;
         try {
             ScheduleJob job = (ScheduleJob) context.getMergedJobDataMap().get("scheduleJob");
             // SpringUtil工具类取到bean对象并通反射调用需要执行的方法
-            Optional.ofNullable(Optional.ofNullable(getBeanByName(job.getSpringId()))
-                    .orElse(getBeanByClazz(Class.forName(job.getClassName())))).ifPresent(
-                    c -> {
-                        try {
-                            (c.getClass().getMethod(job.getMethodName())).invoke(c);
-                        } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
-                            System.out.println(e.getMessage());
-                        }
-                    }
-            );
-
-        } catch (ClassNotFoundException e) {
+            object = getBeanByName(job.getSpringId()) != null
+                    ? getBeanByName(job.getSpringId())
+                    : getBeanByClazz(Class.forName(job.getClassName()));
+            if (object != null) {
+                method = object.getClass().getMethod(job.getMethodName());
+            }
+            if (method != null) {
+                method.setAccessible(true);
+                method.invoke(object);
+            }
+        } catch (ClassNotFoundException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
             System.out.println(e.getMessage());
-
         }
     }
 }
