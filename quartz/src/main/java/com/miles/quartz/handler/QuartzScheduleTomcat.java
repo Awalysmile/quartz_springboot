0b35e81e92fec7f24f2dea3aed4a9609c8ec6073
miles <awalsmile@qq.com>
2019-08-04 17:52:23 +0800

    实现调度

---
+++ quartz/src/main/java/com/miles/quartz/handler/QuartzScheduleTomcat.java
@@ -0,0 +1,21 @@
+package com.miles.quartz.handler;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName QuartzHandlerTomCat
+ * @Description TODO
+ * @Author Miles
+ * @Date 2019/8/4 17:47
+ * @Version 1.0
+ */
+@Component
+public class QuartzScheduleTomcat implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
